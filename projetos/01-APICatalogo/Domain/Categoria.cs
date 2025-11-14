4ea90c14757016f0a0aa06ef43682f27fcf61b4e
Matheus Luna <matheus-deluna@hotmail.com>
2025-11-13 22:57:42 -0300

    Atualização

---
+++ projetos/01-APICatalogo/Domain/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_APICatalogo.Domain;
+
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
